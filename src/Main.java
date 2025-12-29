9389ac1077266dca698fc253a8a70df470cb5e51
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2025-12-29 22:15:24 +0900

    feat: 構造体メニュー1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            String nickname = sc.next();
+            int old = sc.nextInt();
+            String birth = sc.next();
+            String state = sc.next();
+
+            System.out.println("User{");
+            System.out.println("nickname : " + nickname);
+            System.out.println("old : " + old);
+            System.out.println("birth : " + birth);
+            System.out.println("state : " + state);
+            System.out.println("}");
+        }
     }
 }
