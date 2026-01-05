4bb89c1f87126929cd79b3b8d9030541922d7bea
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2026-01-05 23:54:42 +0900

    feat: 出口のない迷路

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,21 @@ import java.util.Scanner;
 class Student {
     String name, birth, state;
     int old;
+
+    void changeName(String name) {
+        this.name = name;
+    }
 }
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // クラスの人数
         int n = sc.nextInt();
+
+        // 名前の更新回数
+        int t = sc.nextInt();
+
         Student[] students = new Student[n];
 
         for (int i = 0; i < n; i++) {
@@ -20,8 +29,17 @@ public class Main {
             students[i].state = sc.next();
         }
 
-        Arrays.sort(students, (a, b) -> a.old - b.old);
+//        for (int i = 0; i < 2; i++) {
+//            System.out.println(infos[i].name);
+//        }
 
+        for (int i = 0; i < t; i++) {
+            int a = sc.nextInt() -1;
+            String nn = sc.next();
+            students[a].changeName(nn);
+        }
+
+        // 最終的な回答の出力
         for (int i = 0; i < n; i++ ) {
             System.out.println(students[i].name + " " + students[i].old + " " + students[i].birth + " " + students[i].state);
         }
---
+++ src/Main2.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+class Point {
+
+    char a;
+    int[] road;
+
+    Point(char a, int r1, int r2) {
+        this.a = a;
+        this.road = new int[] { r1, r2 };
+    }
+}
+
+public class Main2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int s = sc.nextInt();
+
+        Point[] points = new Point[n + 1];
+        for (int i = 1; i <= n; i++) {
+            String a = sc.next();
+            int r1 = sc.nextInt();
+            int r2 = sc.nextInt();
+            points[i] = new Point(a.charAt(0), r1, r2);
+        }
+
+        int now = s;
+        StringBuilder sb = new StringBuilder();
+        sb.append(points[now].a);
+
+        for (int i = 0; i < k; i++) {
+            int m = sc.nextInt() - 1;
+            now = points[now].road[m];
+            sb.append(points[now].a);
+        }
+
+        System.out.println(sb.toString());
+    }
+}
